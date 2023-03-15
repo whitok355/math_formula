446e4d93b295241b206c8ae70b669ab9dbf39959
whitok355 <3139139@mail.ru>
2023-03-15 23:28:57 +0300

    search 1,2,3 numbers

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(100, 1000);
+
+// Поиск первого числа
+int a1 = a / 100;
+Console.WriteLine($"rand{a}, aa{a1}");
+
+// Поиск второго числа
+int a2 = a  % 100 / 10;
+Console.WriteLine($"rand{a}, aa{a2}");
+
+//Поиск третьего числа
+int a3 = a % 10;
+Console.WriteLine($"rand{a}, aa{a3}");
